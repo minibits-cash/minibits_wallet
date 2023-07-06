a2cfacf29381fad538878b1f7f5635030a92d726
minibits-cash <support@minibits.cash>
2023-07-06 23:22:35 +0200

    Lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-};
+  rules: {
+    
+    semi: ['error', 'never'],
+    "unused-imports/no-unused-imports": "error"    
+    
+  },
+  plugins: ['unused-imports']
+}
