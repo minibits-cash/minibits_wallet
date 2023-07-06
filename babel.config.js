332e888793b55b6d287bf6615435bcdf4422e0fe
minibits-cash <support@minibits.cash>
2023-07-06 23:27:43 +0200

    Configs and README

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+        'module-resolver',
+        {
+          alias: {
+            'crypto': 'react-native-quick-crypto',
+            'stream': 'stream-browserify',
+            'buffer': '@craftzdog/react-native-buffer',
+          },
+        },
+    ],
+    'react-native-reanimated/plugin',    
+    'module:react-native-dotenv'
+  ],
 };
