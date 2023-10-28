6610f945f926977e3864c00284321f497365f49e
minibits-cash <support@minibits.cash>
2023-10-28 22:22:39 +0200

    Allow http .onion conns, fix rn- screens crashes

--- android/app/src/main/java/com/minibits_wallet/MainActivity.java
+++ android/app/src/main/java/com/minibits_wallet/MainActivity.java
@@ -1,5 +1,6 @@
 package com.minibits_wallet;
 
+import android.os.Bundle; // react-native-screens fix
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -28,5 +29,11 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }.
+
+  // react-native-screens fix
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
