31e60626ff5b1753c34886d328852518400b19c5
KraXen72 <dpelech1@gmail.com>
2024-06-06 12:48:57 +0200

    support current user's locale automatically, with fallback (#79)

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,23 +1,44 @@
 // import * as Localization from "expo-localization"
-// import { I18nManager } from "react-native"
 import { I18n } from "i18n-js"
+import { NativeModules, Platform } from "react-native"
 
 // if English isn't your default language, move Translations to the appropriate language file.
 import en from "../i18n_messages/en.json"
+import sk from "../i18n_messages/sk.json"
 
 type Translations = typeof en;
-export const i18n = new I18n({'en-US': en})
+export const i18n = new I18n({ 'en-US': en })
 
-// i18n.fallbacks = true
 /**
  * we need always include "*-US" for some valid language codes because when you change the system language,
  * the language code is the suffixed with "-US". i.e. if a device is set to English ("en"),
  * if you change to another language and then return to English language code is now "en-US".
  */
-i18n.translations = { en, "en-US": en }
+i18n.translations = { 
+  en, "en-US": en, 
+  sk
+}
 
-// TODO get current system locale
-i18n.locale = "en-US"
+// important!
+i18n.defaultLocale = 'en'
+i18n.enableFallback = true;
+
+// i wanted to use Intl.Locale to parse this and get the base language
+// however, seems like Intl.Locale is either not implemented in hermes or not workiking for this version
+// also, react native provides locales in the en_US format, whereas js expect them in en-US format
+// this is smaller than a full intl polyfill and does what we need
+
+const fullLocaleRN: string = (Platform.OS === 'ios'
+  ? NativeModules.SettingsManager.settings.AppleLocale
+  : NativeModules.I18nManager.localeIdentifier
+).toString().replaceAll('_', '-')
+
+const localeJSFormat = fullLocaleRN.includes("-") 
+  ? fullLocaleRN.split("-")[0]
+  : fullLocaleRN.slice(0, 2)
+
+i18n.locale = localeJSFormat
+
 
 /**
  * Builds up valid keypaths for translations.
