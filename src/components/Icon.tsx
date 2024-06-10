bd6665a6c9531d70c59481470b5f10b5bf049c94
KraXen72 <dpelech1@gmail.com>
2024-06-10 20:40:50 +0200

    icons: move imports back

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -14,80 +14,81 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { colors, spacing, useThemeColor } from "../theme"
 import { IconDefinition, Transform } from "@fortawesome/fontawesome-svg-core"
 
-import { 
-  faTwitter, 
-  faTelegramPlane,
-  faDiscord,
-  faGithub,
-  faReddit
-} from "@fortawesome/free-brands-svg-icons"
-import {
-  faAddressBook,
-  faAddressCard,
-  faArrowDown,
-  faArrowLeft,
-  faArrowRightToBracket,
-  faArrowTurnDown,
-  faArrowTurnUp,
-  faArrowUp,
-  faArrowUpFromBracket,
-  faBan,
-  faBolt,
-  faBug,
-  faBullseye,
-  faBurst,
-  faCheckCircle,
-  faCircle,
-  faCircleNodes,
-  faCircleUser,
-  faClipboard,
-  faCloudArrowUp,
-  faCode,
-  faCoins,
-  faComment,
-  faCopy,
-  faDownload,
-  faEllipsis,
-  faEllipsisVertical,
-  faExpand,
-  faEyeSlash,
-  faFingerprint,
-  faGears,
-  faInfoCircle,
-  faKey,
-  faKeyboard, 
-  faListUl,
-  faLock,
-  faLockOpen,
-  faMoneyBill1, 
-  faPaintbrush,
-  faPaperPlane,
-  faPaste, 
-  faPencil,
-  faPlus,
-  faQrcode,
-  faRecycle,
-  faRotate,
-  faShareFromSquare,
-  faShareNodes,
-  faShieldHalved,
-  faSliders,
-  faTags,
-  faTriangleExclamation,
-  faUpload,
-  faUpRightFromSquare,
-  faUserShield,
-  faWallet,
-  faWandMagicSparkles,
-  faXmark,
-  faTag,
-  faBank,
-  faChevronDown,
-  faChevronUp,
-  faCircleExclamation,
-  faCircleQuestion,
-  faEnvelope
-} from '@fortawesome/free-solid-svg-icons'
+// due to Metro bundler not currently supporting tree-shaking, we take the safe routes and use "deep imports" for icons
+// metro bundler tree-shaking issue: https://github.com/facebook/metro/issues/132
+// relevant font-awesome docs: https://docs.fontawesome.com/apis/javascript/tree-shaking
+
+import { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter"
+import { faTelegramPlane } from "@fortawesome/free-brands-svg-icons/faTelegramPlane"
+import { faDiscord } from "@fortawesome/free-brands-svg-icons/faDiscord"
+import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub"
+import { faReddit } from "@fortawesome/free-brands-svg-icons/faReddit"
+ 
+import { faWallet } from '@fortawesome/free-solid-svg-icons/faWallet'
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons/faAddressCard'
+import { faAddressBook } from '@fortawesome/free-solid-svg-icons/faAddressBook'
+import { faQrcode } from '@fortawesome/free-solid-svg-icons/faQrcode'
+import { faClipboard } from '@fortawesome/free-solid-svg-icons/faClipboard'
+import { faSliders } from '@fortawesome/free-solid-svg-icons/faSliders'
+import { faCoins } from '@fortawesome/free-solid-svg-icons/faCoins'
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons/faEllipsisVertical'
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons/faEllipsis'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons/faArrowUp'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons/faArrowDown'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft'
+import { faXmark } from '@fortawesome/free-solid-svg-icons/faXmark'
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons/faInfoCircle'
+import { faBug } from '@fortawesome/free-solid-svg-icons/faBug'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons/faCheckCircle'
+import { faArrowTurnUp } from '@fortawesome/free-solid-svg-icons/faArrowTurnUp'
+import { faArrowTurnDown } from '@fortawesome/free-solid-svg-icons/faArrowTurnDown'
+import { faPencil } from '@fortawesome/free-solid-svg-icons/faPencil'
+import { faTags } from '@fortawesome/free-solid-svg-icons/faTags'
+import { faShareFromSquare } from '@fortawesome/free-solid-svg-icons/faShareFromSquare'
+import { faRotate } from '@fortawesome/free-solid-svg-icons/faRotate'
+import { faCode } from '@fortawesome/free-solid-svg-icons/faCode'
+import { faBan } from '@fortawesome/free-solid-svg-icons/faBan'
+import { faCircle } from '@fortawesome/free-solid-svg-icons/faCircle'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons/faPaperPlane'
+import { faBolt } from '@fortawesome/free-solid-svg-icons/faBolt'
+import { faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons/faArrowUpFromBracket'
+import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons/faArrowRightToBracket'
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
+import { faShieldHalved } from '@fortawesome/free-solid-svg-icons/faShieldHalved'
+import { faCloudArrowUp } from '@fortawesome/free-solid-svg-icons/faCloudArrowUp'
+import { faPaintbrush } from '@fortawesome/free-solid-svg-icons/faPaintbrush'
+import { faCopy } from '@fortawesome/free-solid-svg-icons/faCopy'
+import { faBurst } from '@fortawesome/free-solid-svg-icons/faBurst'
+import { faUserShield } from '@fortawesome/free-solid-svg-icons/faUserShield'
+import { faLock } from '@fortawesome/free-solid-svg-icons/faLock'
+import { faLockOpen } from '@fortawesome/free-solid-svg-icons/faLockOpen'
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons/faTriangleExclamation'
+import { faDownload } from '@fortawesome/free-solid-svg-icons/faDownload'
+import { faUpload } from '@fortawesome/free-solid-svg-icons/faUpload'
+import { faRecycle } from '@fortawesome/free-solid-svg-icons/faRecycle'
+import { faListUl } from '@fortawesome/free-solid-svg-icons/faListUl'
+import { faExpand } from '@fortawesome/free-solid-svg-icons/faExpand'
+import { faFingerprint } from '@fortawesome/free-solid-svg-icons/faFingerprint'
+import { faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons/faWandMagicSparkles'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons/faCircleUser'
+import { faComment } from '@fortawesome/free-solid-svg-icons/faComment'
+import { faKey } from '@fortawesome/free-solid-svg-icons/faKey'
+import { faCircleNodes } from '@fortawesome/free-solid-svg-icons/faCircleNodes'
+import { faBullseye } from '@fortawesome/free-solid-svg-icons/faBullseye'
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons/faEyeSlash'
+import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons/faUpRightFromSquare'
+import { faShareNodes } from '@fortawesome/free-solid-svg-icons/faShareNodes'
+import { faPaste } from '@fortawesome/free-solid-svg-icons/faPaste'
+import { faKeyboard } from '@fortawesome/free-solid-svg-icons/faKeyboard'
+import { faMoneyBill1 } from '@fortawesome/free-solid-svg-icons/faMoneyBill1'
+import { faGears } from '@fortawesome/free-solid-svg-icons/faGears'
+import { faTag } from '@fortawesome/free-solid-svg-icons/faTag'
+import { faBank } from '@fortawesome/free-solid-svg-icons/faBank'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons/faChevronDown'
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons/faChevronUp'
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons/faCircleExclamation'
+import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons/faCircleQuestion'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope'
 
 
 export type IconTypes = keyof typeof iconRegistry
