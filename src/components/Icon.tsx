47510b2fc39545c5d4ec460253330e465a32b42c
KraXen72 <dpelech1@gmail.com>
2024-06-10 13:21:38 +0200

    better motd card, better nuts card

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -77,6 +77,8 @@ import {
   faBank,
   faChevronDown,
   faChevronUp,
+  faCircleExclamation,
+  faCircleQuestion
 } from '@fortawesome/free-solid-svg-icons'
 
 
@@ -84,7 +86,7 @@ export type IconTypes = keyof typeof iconRegistry
 
 // TODO remove need for manual iconregistry? 
 // would be best to just import all of them, i guess, or figure out something smart
-export const iconRegistry = { faAddressCard, faAddressBook, faWallet, faQrcode, faClipboard, faSliders, faCoins, faEllipsisVertical, faEllipsis, faArrowUp, faArrowDown, faArrowLeft, faXmark, faInfoCircle, faBug, faCheckCircle, faArrowTurnUp, faArrowTurnDown, faPencil, faTags, faShareFromSquare, faRotate, faCode, faBan, faCircle, faPaperPlane, faBolt, faArrowUpFromBracket, faArrowRightToBracket, faPlus, faShieldHalved, faCloudArrowUp, faPaintbrush, faCopy, faBurst, faUserShield, faLock, faLockOpen, faTriangleExclamation, faDownload, faUpload, faRecycle, faListUl, faExpand, faFingerprint, faWandMagicSparkles, faCircleUser, faComment, faKey, faCircleNodes, faBullseye, faEyeSlash, faUpRightFromSquare, faShareNodes, faPaste, faKeyboard, faMoneyBill1, faGears, faTag, faBank, faChevronDown, faChevronUp }
+export const iconRegistry = { faAddressCard, faAddressBook, faWallet, faQrcode, faClipboard, faSliders, faCoins, faEllipsisVertical, faEllipsis, faArrowUp, faArrowDown, faArrowLeft, faXmark, faInfoCircle, faBug, faCheckCircle, faArrowTurnUp, faArrowTurnDown, faPencil, faTags, faShareFromSquare, faRotate, faCode, faBan, faCircle, faPaperPlane, faBolt, faArrowUpFromBracket, faArrowRightToBracket, faPlus, faShieldHalved, faCloudArrowUp, faPaintbrush, faCopy, faBurst, faUserShield, faLock, faLockOpen, faTriangleExclamation, faDownload, faUpload, faRecycle, faListUl, faExpand, faFingerprint, faWandMagicSparkles, faCircleUser, faComment, faKey, faCircleNodes, faBullseye, faEyeSlash, faUpRightFromSquare, faShareNodes, faPaste, faKeyboard, faMoneyBill1, faGears, faTag, faBank, faChevronDown, faChevronUp, faCircleExclamation, faCircleQuestion }
 
 
 interface IconProps extends TouchableOpacityProps {
@@ -175,7 +177,7 @@ const $imageStyle: ImageStyle = {
 }
 
 const $container: ImageStyle = {    
-    padding: spacing.extraSmall,    
+  padding: spacing.extraSmall,    
 }
 
 const $inverseContainer: ImageStyle = {
--- src/screens/MintInfoScreen.tsx
+++ src/screens/MintInfoScreen.tsx
@@ -1,10 +1,10 @@
 import React, { FC, useEffect, useState } from 'react'
-import { GetInfoResponse } from '@cashu/cashu-ts'
+import { GetInfoResponse, SwapMethod } from '@cashu/cashu-ts'
 import { isObj } from '@cashu/cashu-ts/src/utils'
 import Clipboard from '@react-native-clipboard/clipboard'
 import { observer } from 'mobx-react-lite'
 import { getSnapshot } from 'mobx-state-tree'
-import { LayoutAnimation, Platform, TextStyle, UIManager, View, ViewStyle } from 'react-native'
+import { DimensionValue, LayoutAnimation, Platform, TextStyle, UIManager, View, ViewStyle } from 'react-native'
 import JSONTree from 'react-native-json-tree'
 import {
   $sizeStyles,
@@ -27,11 +27,12 @@ import { useStores } from '../models'
 import { Mint, MintStatus } from '../models/Mint'
 import { SettingsStackScreenProps } from '../navigation'
 import { MintClient, log } from '../services'
-import { colors, spacing, useThemeColor } from '../theme'
+import { colors, spacing, typography, useThemeColor } from '../theme'
 import useColorScheme from '../theme/useThemeColor'
 import AppError, { Err } from '../utils/AppError'
 import { useHeader } from '../utils/useHeader'
 import { CurrencySign } from './Wallet/CurrencySign'
+import { MintUnit, MintUnits } from '../services/wallet/currency'
 
 
 if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
@@ -42,16 +43,27 @@ const iconMap: Partial<Record<keyof GetInfoResponse, IconTypes>> = {
   'name': 'faTag',
   'pubkey': 'faKey',
   'version': 'faInfoCircle',
-  'contact': 'faAddressCard',
-  'motd': 'faPaperPlane'
+  'contact': 'faAddressCard'
 }
 const prettyNamesMap: Partial<Record<keyof GetInfoResponse, string>> = {}
+
+function MOTDCard(props: {info: GetInfoResponse}) {
+  const textDim = useThemeColor('textDim')
+  return (<Card
+    RightComponent={<View style={{ justifyContent: 'center' }}>
+      <Icon icon="faCircleExclamation" color={textDim} size={20} />
+    </View>}
+    headingTx="mintInfo.motd"
+    HeadingTextProps={{ style: [$sizeStyles.sm, {color: textDim}] }}
+    ContentComponent={<Text style={{ fontStyle: 'italic' }} text={props.info.motd} />}
+  />)
+}
 
 function DescriptionCard(props: {info: GetInfoResponse}) {
   const textDim = useThemeColor('textDim')
   return (<Card
     headingTx="mintInfo.descriptionHeading"
-    HeadingTextProps={{style: [$sizeStyles.sm, {color: textDim }]}}
+    HeadingTextProps={{ style: [$sizeStyles.sm, { color: textDim }] }}
     style={$card}
     ContentComponent={
       props.info && props.info.description ? (
@@ -72,12 +84,109 @@ function DescriptionCard(props: {info: GetInfoResponse}) {
   />)
 }
 
-function NutsCard() {
-  // TODO implement
+interface DetailedNutInfo {
+  methods: Array<SwapMethod>;
+  disabled: boolean;
+}
+
+function NutItem(props: {
+  enabled: boolean,
+  nutNumber: string,
+  display: 'row' | 'small',
+  width?: DimensionValue
+  nutInfo?: DetailedNutInfo
+}) {
+  const textDim = useThemeColor('textDim')
+  const $nutItem: ViewStyle = {
+    flexDirection: 'row',
+    width: props.display === 'row' ? '100%' : (props?.width ?? 'auto'),
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    gap: spacing.extraSmall
+  }
+  const $nutIcon: ViewStyle = { paddingHorizontal: 0 }
+
+  return (
+    <View style={$nutItem}>
+      <Icon
+        icon={props.enabled ? 'faCheckCircle' : 'faXmark'}
+        color={props.enabled ? colors.palette.success200 : colors.palette.angry500}
+        size={16}
+        containerStyle={$nutIcon}
+      />
+      <Text size='xs'>NUT-{props.nutNumber}</Text>
+      {props.nutInfo && props.nutInfo.methods.map(m => (<>
+        <Text text={m.method} />
+        {/* <Icon icon='faBolt' color={textDim} size={16} containerStyle={$nutIcon} /> */}
+        {m.min_amount && m.max_amount 
+          ? <Text text={`${m.min_amount} – ${m.max_amount}`} />
+          : m.min_amount && !m.max_amount ? <Text text={`min: ${m.min_amount}`} />
+          : m.max_amount && !m.min_amount ? <Text text={`max: ${m.max_amount}`} />
+          : void 0
+        }
+        {MintUnits.includes(m.unit as MintUnit) ? (
+          <CurrencySign
+            textStyle={{fontSize: 16}}
+            containerStyle={$nutIcon}
+            mintUnit={m.unit as MintUnit}
+          />
+        ) : (<>
+          <Icon icon='faCircleQuestion' color={textDim} size={16} containerStyle={$nutIcon} />
+          <Text text={m.unit.toUpperCase()} style={{fontFamily: typography.primary?.light}} />
+        </>)}
+      </>))}
+    </View>
+  )
+}
+
+function NutsCard(props: {info: GetInfoResponse}) {
+  const textDim = useThemeColor('textDim')
+  const supportedNutsDetailed: [string, DetailedNutInfo][] = []
+  const nutsSimple: [string, boolean][] = []
+
+  for (const [nut, info] of Object.entries(props.info.nuts)) {
+    if ('disabled' in info && info.disabled === false) { // detailed
+      supportedNutsDetailed.push([nut, info])
+    } else if ('supported' in info) { // simple
+      nutsSimple.push([nut, info.supported])
+    } else {
+      nutsSimple.push([nut, false]) // fallback or detailed, but disabled nut
+    }
+  }
+
+  const smallNutCols = 4
+  return (<Card
+    heading="Nuts"
+    HeadingTextProps={{style: [$sizeStyles.sm, {color: textDim}]}}
+    ContentComponent={
+      <>
+        {supportedNutsDetailed.map(([nut, info]) => (
+          <NutItem
+            nutNumber={nut}
+            enabled={true}
+            display="row"
+            key={nut}
+            nutInfo={info}
+          />
+        ))}
+        <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+          {nutsSimple.map(([nut, enabled]) => (
+            <NutItem
+              nutNumber={nut}
+              enabled={enabled}
+              key={nut}
+              display="small"
+              width={`${Math.round(100 / smallNutCols)}%`}
+            />
+          ))}
+        </View>
+      </>
+    }
+  />)
 }
 
 /** don't render these because they're rendered in separate components */
-const detailsHiddenKeys = ['name', 'description', 'description_long', 'motd']
+const detailsHiddenKeys = new Set(['name', 'motd', 'description', 'description_long', 'nuts'])
 
 /** key-value pairs of details about the mint */
 function MintInfoDetails(props: { info: GetInfoResponse, popupMessage: (msg: string) => void }) {
@@ -85,7 +194,7 @@ function MintInfoDetails(props: { info: GetInfoResponse, popupMessage: (msg: str
   const contactPlatformColor = useThemeColor('textDim')
 
   const items: React.JSX.Element[] = Object.entries(props.info)
-    .filter(([key, value]) => !(detailsHiddenKeys.includes(key)))
+    .filter(([key, value]) => !(detailsHiddenKeys.has(key)))
     .map(([key, value], index) => {
       const missingComponent = <Text
         style={{fontStyle: 'italic'}}
@@ -225,7 +334,6 @@ export const MintInfoScreen: FC<SettingsStackScreenProps<'MintInfo'>> = observer
   const textDim = useThemeColor('textDim')
   return (
     <Screen style={$screen} preset="scroll">
-      {/* <View style={[$headerContainer, { backgroundColor: headerBg }]}> <Text preset="heading" tx="mintInfoHeading" style={{ color: 'white' }} /> </View> */}
       <AvatarHeader
         encircle={true}
         fallbackIcon="faBank"
@@ -251,21 +359,11 @@ export const MintInfoScreen: FC<SettingsStackScreenProps<'MintInfo'>> = observer
         )}
       </AvatarHeader>
       <View style={$contentContainer}>
-        {mintInfo?.motd && (
-          <Card
-            LeftComponent={
-              <View style={{justifyContent: 'center'}}>
-                <Icon icon="faPaperPlane" color={textDim} size={26} />
-              </View>
-            }
-            headingTx="mintInfo.motd"
-            HeadingTextProps={{style: [$sizeStyles.sm, {color: textDim}]}}
-            ContentComponent={
-              <Text style={{fontStyle: 'italic'}} text={mintInfo.motd} />
-            }
-          />
-        )}
-        {mintInfo && <DescriptionCard info={mintInfo} />}
+        {mintInfo?.motd && <MOTDCard info={mintInfo} />}
+        {mintInfo && <>
+          <DescriptionCard info={mintInfo} />
+          <NutsCard info={mintInfo} />
+        </>}
         <Card
           ContentComponent={
             <>
