665545ffb542db683b48fc384f6d8de56d0b6b82
minibits-cash <mail@minibits.cash>
2025-08-03 23:40:18 +0200

    Upgrade to RN 0.80.2 and libs upgrade

--- android/app/src/main/java/com/minibits_wallet/MainApplication.kt
+++ android/app/src/main/java/com/minibits_wallet/MainApplication.kt
@@ -4,15 +4,13 @@ import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
-import com.facebook.soloader.SoLoader
 import com.facebook.react.modules.network.OkHttpClientProvider
-// import com.microsoft.codepush.react.CodePush
 import com.hotupdater.HotUpdater
 
 class MainApplication : Application(), ReactApplication {
@@ -49,13 +47,8 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-
+    
     OkHttpClientProvider.setOkHttpClientFactory(CorePlaneOkHttpClientFactory())
-
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
+    loadReactNative(this)
   }
 }
